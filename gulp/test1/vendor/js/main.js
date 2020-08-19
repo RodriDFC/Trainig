864353084cf05b663b5bbf91f54853189e2f195a
RodriDFC <rodrifnts@gmail.com>
2020-08-19 16:37:42 -0400

    mejorando test 1 en gulp

--- gulp/test1/vendor/js/main.js
+++ gulp/test1/vendor/js/main.js
@@ -1,4 +1,29 @@
 $(document).ready(function () {
+    $('.section--team__cards').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        variableWidth: true,
+        arrows: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,//3
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 760,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
     $('.section--slick__slickSlider').slick({
         dots: true,
         infinite: true,
