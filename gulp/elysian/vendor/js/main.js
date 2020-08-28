c16c6c6bc5024236a131c3b8342f8b9bdc8f4d82
RodriDFC <rodrifnts@gmail.com>
2020-08-28 11:45:05 -0400

    elysian terminado corregido

--- gulp/elysian/vendor/js/main.js
+++ gulp/elysian/vendor/js/main.js
@@ -24,4 +24,29 @@ $(document).ready(function () {
             }
         ]
     });
+    $('.section--goals__g').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        variableWidth: true,
+        arrows: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,//3
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 760,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
 });
